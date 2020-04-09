f8d2f4501a5d93249a4163f3bb1cba4b74c36af7
ustinianyang <62502262+ustinianyang@users.noreply.github.com>
2020-04-09 17:58:38 +0800

    Update lhie1.ex

--- lhie1.ex
+++ lhie1.ex
@@ -1,23 +1,19 @@
-;设置规则标志位
-surge_ruleset=DIRECT,rules/LocalAreaNetwork.list
-surge_ruleset=DIRECT,rules/ConnersHua/Surge/Ruleset/Unbreak.list
-surge_ruleset=🛑 全球拦截,rules/NobyDa/Surge/AdRule.list
-surge_ruleset=全球拦截,rules/ConnersHua/Surge/Ruleset/Hijacking.list
+  ;设置规则标志位
 surge_ruleset=GlobalTV,rules/lhie1/Surge3/GlobalTV.list
 surge_ruleset=AsianTV,rules/lhie1/Surge3/AsianTV.list
+surge_ruleset=Netease Music,rules/lhie1/Surge3/Netease Music.list
 surge_ruleset=Proxy,rules/lhie1/Surge3/Proxy.list
 surge_ruleset=Domestic,rules/lhie1/Surge3/Domestic.list
-surge_ruleset=DIRECT,rules/ConnersHua/Surge/Ruleset/China.list
-surge_ruleset=DIRECT,rules/NobyDa/Surge/Download.list
-surge_ruleset=DIRECT,[]GEOIP,CN
-surge_ruleset=Others,[]FINAL
+surge_ruleset=DIRECT,rules/LocalAreaNetwork.list
+surge_ruleset=Domestic,[]GEOIP,CN
+surge_ruleset=Others,[]MATCH
 ;设置规则标志位
 
 ;设置分组标志位
 custom_proxy_group=Proxy`select`.*
 custom_proxy_group=Domestic`select`[]DIRECT`[]Proxy`.*
 custom_proxy_group=AsianTV`select`[]DIRECT`[]Domestic`[]Proxy`.*
+custom_proxy_group=Netease Music`select`[]DIRECT`[]Domestic`[]Proxy`.*
 custom_proxy_group=GlobalTV`select`[]Proxy`.*
-custom_proxy_group=🛑 全球拦截`select`[]REJECT`[]DIRECT
 custom_proxy_group=Others`select`[]Proxy`[]DIRECT`.*
 ;设置分组标志位
